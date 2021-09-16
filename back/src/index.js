10ceffa84e7a589e43a281dea5d799cb28849ab7
Leonardo Bellaera <lbellaera@fi.uba.ar>
2021-09-16 15:57:36 -0300

    Adds mongo service integration to local and prod envs (#3)

--- back/src/index.js
+++ back/src/index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,17 @@ app.listen(port, () => {
   console.log(`App listening at port ${port}`);
 });
 
-app.get('/', (req, res) => {
-  res.send("Hello world!")
-})
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "db connection error: "));
+db.once("open", function () {
+  console.log("db connected successfully");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
