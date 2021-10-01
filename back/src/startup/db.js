6f9deae3e6c3f79379d8169d0e5eb8f61201a03c
seblaz <sebastian.blazquez96@gmail.com>
2021-10-01 20:27:40 -0300

    Agrego mongo express y tageo imagenes de docker

--- back/src/startup/db.js
+++ back/src/startup/db.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 module.exports = function connectToMongo() {
   mongoose.connect(process.env.DATABASE_URL, {
+    auth: {
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+    },
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
