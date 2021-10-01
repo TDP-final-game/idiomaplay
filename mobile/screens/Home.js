5acea0220e10df0d8be6413a0c271dac3a70f5a5
facutorraca <ftorraca@fi.uba.ar>
2021-09-30 22:44:33 -0300

    bottom slider example

--- mobile/screens/Home.js
+++ mobile/screens/Home.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { colors } from '../config/colors';
-import { View, StyleSheet, Button, Text } from 'react-native';
+import { View, StyleSheet, Button, Text, TouchableHighlight, Animated } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = ({ navigation }) => {
+  const [isHidden, setHidden] = useState(true);
+  const [buttonText, setButtonText] = useState('Show Subview');
+  const [bounceValue, setBounceValue] = useState(new Animated.Value(100));
+
+  const _toggleSubview = () => {
+    setButtonText(!isHidden ? 'Show Subview' : 'Hide Subview');
+
+    var toValue = 100;
+    if (isHidden) toValue = 0;
+
+    // This will animate the transalteY of the subview
+    // between 0 & 100 depending on its current state
+    // 100 comes from the style below, which is the height
+    // of the subview.
+    Animated.spring(bounceValue, {
+      useNativeDriver: true,
+      toValue: toValue,
+      velocity: 3,
+      tension: 2,
+      friction: 8,
+    }).start();
+
+    setHidden(!isHidden);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.buttonContainer}>
@@ -12,6 +37,18 @@ const Home = ({ navigation }) => {
           onPress={() => navigation.navigate('Excercise')}
         />
       </View>
+
+      <TouchableHighlight
+        style={styles.button}
+        onPress={() => {
+          _toggleSubview();
+        }}
+      >
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableHighlight>
+      <Animated.View style={[styles.subView, { transform: [{ translateY: bounceValue }] }]}>
+        <Text>This is a sub view</Text>
+      </Animated.View>
     </SafeAreaView>
   );
 };
@@ -27,6 +64,22 @@ const styles = StyleSheet.create({
     flex: 0.07,
     marginHorizontal: '5%',
     justifyContent: 'center',
+  },
+
+  button: {
+    padding: 8,
+  },
+  buttonText: {
+    fontSize: 17,
+    color: '#007AFF',
+  },
+  subView: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#FFFFFF',
+    height: 100,
   },
 });
 
