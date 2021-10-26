b615be197bab6b5f32733b93c9fa7eb9d8ce407a
Magui <maguimar001@gmail.com>
2021-10-25 22:35:08 -0300

    fixes for demo

--- back/src/controllers/userController.js
+++ back/src/controllers/userController.js
@@ -28,11 +28,12 @@ const createUser = async (req, res) => {
 		throw new ApiError(ApiError.codes.BAD_REQUEST, 'User is required');
 
 	const { firstName, lastName } = req.body;
-	res.status(STATUS_CODES.CREATED).send(await userService.createUser({
+	const userCreated = await userService.createUser({
 		email: user.email,
 		firstName,
 		lastName
-	}));
+	});
+	res.status(STATUS_CODES.CREATED).send({ id: userCreated._id });
 };
 
 const logIn = async (req, res) => {
@@ -40,7 +41,7 @@ const logIn = async (req, res) => {
 
 	const { user } = req;
 	const response = await userService.logIn({ email: user.email });
-	res.status(STATUS_CODES.OK).send(response);
+	res.status(STATUS_CODES.OK).send({ id: response._id });
 };
 
 module.exports = {
