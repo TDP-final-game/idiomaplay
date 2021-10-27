a59c7c281894ad2dfd6f0975491aa3663dc91eba
lbellaera <lbellaera@fi.uba.ar>
2021-10-27 19:06:46 -0300

    options amount fix

--- back/src/model/exercises/exercise.js
+++ back/src/model/exercises/exercise.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const exerciseInfo = require('./exerciseInfo');
 const STATUSES = require('../../constants/statuses.json');
 const { model: ExerciseAttempt } = require('../attempts/exerciseAttempt');
+const exerciseTypes = require('../../constants/exerciseTypes');
 
 /*
  * Schema
@@ -15,9 +16,16 @@ const Exercise = new mongoose.Schema(exerciseInfo);
  * Instance methods
  */
 Exercise.methods.newAttempt = function() {
-	// todo: when adding a new exercise to lesson or exam check that there is one and only one correct answer and at least one incorrect answer
 	const incorrectAnswersShuffled = this.options.filter(option => option.correct === false).sort(() => 0.5 - Math.random());
-	const selected = incorrectAnswersShuffled.slice(0, 3);
+
+	let wrongOptionsAmount;
+	if (this.type === exerciseTypes.COMPLETE_SENTENCE) {
+		wrongOptionsAmount = 3;
+	} else {
+		wrongOptionsAmount = 5;
+	}
+
+	const selected = incorrectAnswersShuffled.slice(0, wrongOptionsAmount);
 	const correctAnswer = this.options.filter(option => option.correct === true);
 	const options = selected.concat(correctAnswer).sort(() => 0.5 - Math.random());
 
