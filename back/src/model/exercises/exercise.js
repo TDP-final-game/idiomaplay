3fa4bcecbfd8451b345728b0b7d8a59efc6daac3
lbellaera <lbellaera@fi.uba.ar>
2021-10-27 16:37:38 -0300

    Select three random incorrect options on new exercise attempt and the correct one

--- back/src/model/exercises/exercise.js
+++ back/src/model/exercises/exercise.js
@@ -15,8 +15,16 @@ const Exercise = new mongoose.Schema(exerciseInfo);
  * Instance methods
  */
 Exercise.methods.newAttempt = function() {
+	// todo: when adding a new exercise to lesson or exam check that there is one and only one correct answer and at least one incorrect answer
+	const incorrectAnswersShuffled = this.options.filter(option => option.correct === false).sort(() => 0.5 - Math.random());
+	const selected = incorrectAnswersShuffled.slice(0, 3);
+	const correctAnswer = this.options.filter(option => option.correct === true);
+	const options = selected.concat(correctAnswer).sort(() => 0.5 - Math.random());
+
 	return new ExerciseAttempt({
-		...this.toObject(),
+		type: this.type,
+		statement: this.statement,
+		options,
 		status: STATUSES.PENDING
 	});
 };
