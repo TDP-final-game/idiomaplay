08e9e02c88dc9c0452ae1c12bf547cb2b4bc4960
Magui <maguimar001@gmail.com>
2021-11-05 00:15:37 -0300

    fixes (#50)

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -5,13 +5,13 @@ const express = require('express');
 const userController = require('../controllers/userController');
 
 const errorHandler = require('../controllers/middlewares/errorHandler');
-
+const middlewares = require('../controllers/middlewares/index');
 
 const router = express.Router();
 
 router.post('/', errorHandler, userController.createUser);
 router.post('/session', errorHandler, userController.logIn);
-router.get('/me/challengeAttempts', errorHandler, userController.listChallengesAttempts);
-router.get('/me/stats', errorHandler, userController.getStats);
+router.get('/me/challengeAttempts', middlewares, userController.listChallengesAttempts);
+router.get('/me/stats', middlewares, userController.getStats);
 
 module.exports = router;
