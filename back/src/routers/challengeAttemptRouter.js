7bc1074835d6789baeaf0a92bfc72359247712a0
facutorraca <ftorraca@fi.uba.ar>
2021-11-07 16:23:36 -0300

    fix linter

--- back/src/routers/challengeAttemptRouter.js
+++ back/src/routers/challengeAttemptRouter.js
@@ -13,7 +13,8 @@ router.post('/', challengeAttemptController.attemptChallenge);
 router.put('/:challengeAttemptId/unitsAttempts', challengeAttemptController.attemptUnit);
 router.put('/:challengeAttemptId/unitsAttempts/:unitOrderNumber/examAttempts', challengeAttemptController.attemptExam);
 router.put('/:challengeAttemptId/unitsAttempts/:unitOrderNumber/lessonsAttempts', challengeAttemptController.attemptLesson);
-router.put('/:challengeAttemptId/unitsAttempts/:unitOrderNumber/lessonsAttempts/:lessonOrderNumber/exercisesAttempts', challengeAttemptController.attemptLessonExercise);
+router.put('/:challengeAttemptId/unitsAttempts/:unitOrderNumber/lessonsAttempts/:lessonOrderNumber/exercisesAttempts', 
+	challengeAttemptController.attemptLessonExercise);
 router.put('/:challengeAttemptId/unitsAttempts/:unitOrderNumber/examAttempt/exercisesAttempts', challengeAttemptController.attemptExamExercise);
 router.get('/:challengeAttemptId', challengeAttemptController.getChallenge);
 router.get('/:challengeAttemptId/unitsAttempts/:unitOrderNumber', challengeAttemptController.getUnit);
