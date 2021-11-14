4adc3edfa9e6c2cdbc45c9bb6b81629e140c4a13
seblaz <sebastian.blazquez96@gmail.com>
2021-11-14 11:57:21 -0300

    Automate deploy

--- backoffice/src/App.test.js
+++ backoffice/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/react/i);
+  act(() => {
+    render(<App />);
+  })
+  const linkElement = screen.getByText(/export/i);
   expect(linkElement).toBeInTheDocument();
 });
