5adf79b95d3f8a327ad08a053b88d1d3ae4f5763
seblaz <sebastian.blazquez96@gmail.com>
2021-11-18 19:07:22 -0300

    Traducción al español

---
+++ backoffice/src/translations.js
@@ -0,0 +1,14 @@
+const modelMessages = {
+	resources: {
+		users: {
+			name: 'Usuario |||| Usuarios',
+			fields: {
+				firstName: 'Nombre',
+				lastName: 'Apellido',
+				enabled: 'Habilitado',
+			},
+		}
+	}
+}
+
+export default modelMessages;
