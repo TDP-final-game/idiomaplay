b108bea14464d6d20c76a8bd0fb933c3d1246c06
Magui <maguimar001@gmail.com>
2021-11-17 23:27:57 -0300

    add changes

--- mobile/services/userService.js
+++ mobile/services/userService.js
@@ -2,8 +2,8 @@ import api, { authenticate } from './api';
 import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
 
-async function createUser(firstName, lastName, email) {
 
+async function getExpoPushToken() {
   let experienceId = undefined;
   if (!Constants.manifest) {
     // Absence of the manifest means we're in bare workflow
@@ -13,13 +13,20 @@ async function createUser(firstName, lastName, email) {
     experienceId,
   });
 
-  const response = await api.post('/users', { firstName, lastName, email, expoPushToken:  expoPushToken.data });
+  return expoPushToken.data;
+}
+async function createUser(firstName, lastName, email) {
+
+  const expoPushToken = await getExpoPushToken();
+  const response = await api.post('/users', { firstName, lastName, email, expoPushToken:  expoPushToken });
   authenticate(response.data.id);
   return response.data;
 }
 
 async function logIn(email) {
-  const response = await api.post('/users/session', { email });
+
+  const expoPushToken = await getExpoPushToken();
+  const response = await api.post('/users/session', { email,  expoPushToken:  expoPushToken });
   authenticate(response.data.id);
   return response.data;
 }
