6625b830c63e31b87285b6ce023e9872100c4f87
seblaz <sebastian.blazquez96@gmail.com>
2021-11-18 18:53:45 -0300

    Remove exporter

--- backoffice/src/components/users/List.js
+++ backoffice/src/components/users/List.js
@@ -1,7 +1,7 @@
 import { List, Datagrid, EmailField, TextField, BooleanField, EditButton } from 'react-admin';
 
 const UserList = props => (
-	<List {...props} bulkActionButtons={false}>
+	<List {...props} bulkActionButtons={false} exporter={false}>
 		<Datagrid rowClick="edit">
 			<EmailField source="email" />
 			<TextField source="firstName" />
