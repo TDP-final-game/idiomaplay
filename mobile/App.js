781fc1275660a31da885917e8e06eddcc47ada8a
Magui <maguimar001@gmail.com>
2021-11-19 17:46:00 -0300

    add fixes

--- mobile/App.js
+++ mobile/App.js
@@ -82,7 +82,7 @@ Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: true,
-    shouldSetBadge: false,
+    shouldSetBadge: true,
   }),
 });
 
