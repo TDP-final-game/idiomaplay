c7d43f57a9fa67561c634a7f32d8fe69112b1908
lbellaera <lbellaera@fi.uba.ar>
2021-12-02 18:59:16 -0300

    filter by language

--- back/src/model/challenges/challengeInfo.js
+++ back/src/model/challenges/challengeInfo.js
@@ -12,5 +12,11 @@ module.exports = {
 	description: {
 		type: String,
 		required: [true, 'description is required']
+	},
+	language: {
+		type: String,
+		lowercase: true,
+		trim: true,
+		required: [true, 'language is required']
 	}
 };
