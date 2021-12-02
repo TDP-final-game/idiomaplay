a9d0db525a55ed6e4d9e0471e86c3834c97a7fa1
seblaz <sebastian.blazquez96@gmail.com>
2021-12-01 22:22:04 -0300

    Add backoffice authentication

--- back/src/routers/adminUserRouter.js
+++ back/src/routers/adminUserRouter.js
@@ -6,8 +6,6 @@ const adminUserController = require('../controllers/adminUserController');
 
 const router = express.Router();
 
-router.post('/', adminUserController.createUser);
-router.get('/', adminUserController.list);
 router.post('/session', adminUserController.logIn);
 
 module.exports = router;
--- back/src/services/admin/errors.js
+++ back/src/services/admin/errors.js
@@ -3,6 +3,5 @@
 const ApiError = require('../../apiError');
 
 module.exports = {
-	UserAlreadyRegistered: () => new ApiError(ApiError.codes.CONFLICT, 'User already registered'),
-	UserNotRegistered: () => new ApiError(ApiError.codes.UNAUTHORIZED, 'User not registered')
+	UserNotRegistered: () => new ApiError(ApiError.codes.UNAUTHORIZED, 'Usuario no registrado')
 };
