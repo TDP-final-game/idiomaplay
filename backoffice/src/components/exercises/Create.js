e750fb95ea2b2a0703ce97c88a0c334fb4368768
seblaz <sebastian.blazquez96@gmail.com>
2021-12-18 17:24:50 -0300

    Validations

--- backoffice/src/components/exercises/Create.js
+++ backoffice/src/components/exercises/Create.js
@@ -6,43 +6,63 @@ import {
 	RadioButtonGroupInput,
 	ArrayInput,
 	SimpleFormIterator,
-	BooleanInput
+	BooleanInput, required, maxLength
 } from 'react-admin';
 
 import { useLocation } from 'react-router';
 
+const optionsValidations = (value, allValues) => {
+	if (!allValues?.type) {
+		return undefined
+	}
+	if (!value) {
+		return 'Se deben indicar al menos seis opciones';
+	}
+	if (allValues.type === 'complete_sentence' && value.length < 4) {
+		return 'Se deben indicar al menos cuatro opciones';
+	}
+	if (allValues.type !== 'complete_sentence' && value.length < 6) {
+		return 'Se deben indicar al menos seis opciones';
+	}
+
+	const numberOfCorrectAnswers = value.filter(option => option?.correct).length
+	if (numberOfCorrectAnswers !== 1) {
+		return 'Se debe indicar una única opción correcta'
+	}
+	return undefined;
+}
+
 const ExerciseCreate = props => {
-    const location = useLocation();
-    const { challengeId, unitOrderNumber, lessonOrderNumber } = location.state.record;
+	const location = useLocation();
+	const {challengeId, unitOrderNumber, lessonOrderNumber} = location.state.record;
 
-    const auxUrl = `challenges-${challengeId}-units-${unitOrderNumber}-lessons-${lessonOrderNumber}`;
-    const redirect = `/lessons/${auxUrl}/show`;
+	const auxUrl = `challenges-${challengeId}-units-${unitOrderNumber}-lessons-${lessonOrderNumber}`;
+	const redirect = `/lessons/${auxUrl}/show`;
 
-
-    return (
-        <Create {...props}>
+	return (
+		<Create {...props}>
 			<SimpleForm redirect={redirect} initialValues={{challengeId, unitOrderNumber, lessonOrderNumber}}>
-		   		<TextInput source="challengeId" label="Identificador del Desafio" disabled/>
+				<TextInput source="challengeId" label="Identificador del Desafio" disabled/>
 				<TextInput source="unitOrderNumber" label="Número de orden de la Unidad" disabled/>
 				<TextInput source="lessonOrderNumber" label="Número de orden de la Leccion" disabled/>
-				<TextInput source="statement" fullWidth/>
+				<TextInput source="statement" fullWidth validate={[required(), maxLength(100)]}/>
 
 				<RadioButtonGroupInput source="type" choices={[
 					{id: 'listening', name: 'Escuchar audio'},
 					{id: 'complete_sentence', name: 'Completar la frase'},
 					{id: 'translate_to_native', name: 'Traducir a idioma nativo'},
 					{id: 'translate_to_foreign', name: 'Traducir a idioma extranjero'},
-				]}/>
+				]} validate={[required()]}/>
 
-                <ArrayInput source="options">
+				<ArrayInput source="options" validate={[optionsValidations]}>
 					<SimpleFormIterator disableReordering>
-						<TextInput source="text" label="Opcion" fullWidth/>
+						<TextInput source="text" label="Opción" fullWidth validate={[required(), maxLength(100)]}/>
 						<BooleanInput source="correct" label="Correcta"/>
 					</SimpleFormIterator>
 				</ArrayInput>
 			</SimpleForm>
-        </Create>
-    );
+		</Create>
+	);
 };
 
 export default ExerciseCreate;
--- backoffice/src/components/lessons/Create.js
+++ backoffice/src/components/lessons/Create.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import {
-    Create,
-    SimpleForm,
-    TextInput,
+	Create, maxLength, maxValue, minValue, number, required,
+	SimpleForm,
+	TextInput,
 } from 'react-admin';
 
 import { useLocation } from 'react-router';
 
 const LessonCreate = props => {
-    const location = useLocation();
-    const { challengeId, unitOrderNumber } =  location.state.record;
-    
-    const auxUrl = `challenges-${challengeId}-units-${unitOrderNumber}`;
-    const redirect = `/units/${auxUrl}/show`;
-   
-    return (
-        <Create {...props}>
-            <SimpleForm redirect={redirect} initialValues={{challengeId, unitOrderNumber}}>
+	const location = useLocation();
+	const {challengeId, unitOrderNumber} = location.state.record;
+
+	const auxUrl = `challenges-${challengeId}-units-${unitOrderNumber}`;
+	const redirect = `/units/${auxUrl}/show`;
+
+	return (
+		<Create {...props}>
+			<SimpleForm redirect={redirect} initialValues={{challengeId, unitOrderNumber}}>
 				<TextInput source="challengeId" label="Identificador del Desafio" disabled/>
-                <TextInput source="unitOrderNumber" label="Número de orden de la Unidad" disabled/>
-                <TextInput source="orderNumber"/>
-				<TextInput source="name"/>
-				<TextInput source="description" />
-            </SimpleForm>
-        </Create>
-    );
+				<TextInput source="unitOrderNumber" label="Número de orden de la Unidad" disabled/>
+				<TextInput source="orderNumber" validate={[number(), minValue(1), maxValue(50)]}/>
+				<TextInput source="name" validate={[required(), maxLength(50)]}/>
+				<TextInput source="description" validate={[required(), maxLength(100)]}/>
+			</SimpleForm>
+		</Create>
+	);
 };
 
 export default LessonCreate;
