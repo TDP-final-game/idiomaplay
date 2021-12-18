7f5059dfe81e31bd52cbfc0e750e72d71d484489
Sebastián Blázquez <sebastian.blazquez96@gmail.com>
2021-12-18 17:28:27 -0300

    Validations (#113)

--- backoffice/src/components/challenges/Create.js
+++ backoffice/src/components/challenges/Create.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import {
-    Create,
-    SimpleForm,
-    TextInput,
+	Create, maxLength, required,
+	SimpleForm,
+	TextInput,
 } from 'react-admin';
 
 const ChallengeCreate = props => {
-    const redirect = '/challenges';
+	const redirect = '/challenges';
 
-    return (
-        <Create {...props}>
-            <SimpleForm redirect={redirect}>
-				<TextInput source="name"/>
-				<TextInput source="description" />
-                <TextInput source="difficulty" />
-                <TextInput source="language" />
-            </SimpleForm>
-        </Create>
-    );
+	return (
+		<Create {...props}>
+			<SimpleForm redirect={redirect}>
+				<TextInput source="name" validate={[required(), maxLength(50)]}/>
+				<TextInput source="description" validate={[required(), maxLength(100)]}/>
+				<TextInput source="difficulty" validate={[required(), maxLength(50)]}/>
+				<TextInput source="language" validate={[required(), maxLength(50)]}/>
+			</SimpleForm>
+		</Create>
+	);
 };
 
 export default ChallengeCreate;
